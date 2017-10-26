d2908222b3a73bcde67d25527923c049a217e02a
ckenley <ckenley@zebra.com>
2017-10-26 15:08:16 -0500

    some more solutions

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,14 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+}
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +16,18 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> = DateIterator(this)
+    operator fun contains(d: MyDate): Boolean = start <= d && d <= endInclusive
+}
+
+class DateIterator(val dateRange: DateRange) : Iterator<MyDate> {
+    var current = dateRange.start
+    override fun hasNext(): Boolean = current <= dateRange.endInclusive
+
+    override fun next(): MyDate {
+        val result = current
+        current = current.addTimeIntervals(TimeInterval.DAY, 1)
+        return result
+    }
+}
